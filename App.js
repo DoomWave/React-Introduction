56908c6d22d699ca05e2c08aace5a495c91be9e4
DoomWave <acunanajair9@gmail.com>
2024-10-02 19:21:01 -0700

    Detecting platform.

--- App.js
+++ App.js
@@ -1,36 +1,15 @@
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context"
 import { style } from './App.Style';
 import {ProfileCard} from "./components/ProfileCard/ProfileCard"
-import { Alert,Linking,Text } from "react-native";
+import { Alert,Linking,Platform,Text } from "react-native";
 import {useState} from "react";
 
 export default function App() {
-  // Linking.openURL("http://")
-
-  function goToSocialMedia(socialMedia) {
-    let url
-    switch(socialMedia){
-      case "twitter" :
-        url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-        break;
-      case "github":
-        url = "https://github.com/DoomWave";
-        break;
-      case "linkedin":
-        url = "https://github.com/DoomWave";
-        break;
-    }
-    Linking.openURL(url);
-  }
   return(
     <SafeAreaProvider>
       <SafeAreaView style={style.container}>
-        <ProfileCard 
-         onPressSocialMediaIcon={goToSocialMedia}
-         firstName={"Doom"} 
-         lastName={"Wave"} 
-         age={30} 
-        />
+        {Platform.OS === "android" &&  <Text>Hi on Android</Text>}
+        {Platform.OS === "ios" && <Text>Hi on IOS</Text>}
       </SafeAreaView>
     </SafeAreaProvider>
   );
