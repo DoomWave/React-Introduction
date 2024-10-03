8194fc02b2abfe90056657b816fab1dd00b70785
DoomWave <acunanajair9@gmail.com>
2024-10-02 19:18:18 -0700

    Learned about making links an cases.

--- App.js
+++ App.js
@@ -1,26 +1,36 @@
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context"
 import { style } from './App.Style';
 import {ProfileCard} from "./components/ProfileCard/ProfileCard"
-import { Alert } from "react-native";
+import { Alert,Linking,Text } from "react-native";
 import {useState} from "react";
-import { Text } from "react-native";
 
 export default function App() {
-  const [countClick, setCountClick] = useState(0)
-  function hello(name){
-    Alert.alert("Hello "+ name);
-    setCountClick(countClick + 1)
+  // Linking.openURL("http://")
+
+  function goToSocialMedia(socialMedia) {
+    let url
+    switch(socialMedia){
+      case "twitter" :
+        url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        break;
+      case "github":
+        url = "https://github.com/DoomWave";
+        break;
+      case "linkedin":
+        url = "https://github.com/DoomWave";
+        break;
+    }
+    Linking.openURL(url);
   }
   return(
     <SafeAreaProvider>
       <SafeAreaView style={style.container}>
         <ProfileCard 
-         onPressTitle={hello} 
+         onPressSocialMediaIcon={goToSocialMedia}
          firstName={"Doom"} 
          lastName={"Wave"} 
          age={30} 
         />
-        <Text>You clicked {countClick} times on the title</Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- components/ProfileCard/ProfileCard.jsx
+++ components/ProfileCard/ProfileCard.jsx
@@ -6,12 +6,8 @@ export function ProfileCard
      firstName, 
      lastName, 
      age, 
-     isOpenToWork, 
-     onPressTitle
+     onPressSocialMediaIcon
     }) {
-function onPressTitle_(){
-    onPressTitle(firstName+lastName)
-}
     return (
     <View Style={s.container}>
         <View style={s.header}>
@@ -22,32 +18,33 @@ function onPressTitle_(){
           />
          </View>
          <View style={s.texts}>
-            <TouchableOpacity onPress={onPressTitle_}>
           <Text style={s.name}>
-            {firstName}
-            {lastName}
+            {firstName}{lastName}
             </Text>
-            </TouchableOpacity>
            <Text>
              Hi I am React native inter-medium, I am {age} years old, Lets get the 
              touch, and soon.
            </Text>
-
-            <Text style={{backgroundColor: isOpenToWork ? "green": 
-            "red", color: "white"}}>
-            {isOpenToWork ? "I am open to work" : "Not looking for a job"}
-            </Text>
-
          </View>
         </View>
+
         <View style={s.social}>
-            <TouchableOpacity style={s.socialBtn}>
+            <TouchableOpacity 
+            onPress={() => onPressSocialMediaIcon("twitter")} 
+            style={s.socialBtn}
+            >
             <FontAwesome name="twitter" size={24} color="#1DA1F2"/>
             </TouchableOpacity>
-            <TouchableOpacity style={s.socialBtn}>
+            <TouchableOpacity 
+            onPress={() => onPressSocialMediaIcon("linkedin")} 
+            style={s.socialBtn}
+            >
             <FontAwesome name="linkedin-square" size={24} color="#0A66C2"/>
             </TouchableOpacity>
-            <TouchableOpacity style={s.socialBtn}>
+            <TouchableOpacity  
+            onPress={() => onPressSocialMediaIcon("github")} 
+            style={s.socialBtn}
+            >
             <FontAwesome name="github" size={24} color="#333"/>
             </TouchableOpacity>
         </View>
